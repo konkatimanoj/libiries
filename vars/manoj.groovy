8139e167e94dbe5ee792d52b751e1a7cfc34c4e1
konkatimanoj <162788980+konkatimanoj@users.noreply.github.com>
2024-03-22 09:57:28 +0530

    Create manoj.groovy

---
+++ vars/manoj.groovy
@@ -0,0 +1,16 @@
+def download(repo)
+{
+   git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def build()
+{
+  sh "mvn package"
+}
+def deploy("jobname","ip","appname")
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runtesting()
+{
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
