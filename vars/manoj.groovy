11e02993f236e51c7a32480c6ea2c45b8fba02b3
konkatimanoj <162788980+konkatimanoj@users.noreply.github.com>
2024-03-22 10:08:49 +0530

    Update manoj.groovy

--- vars/manoj.groovy
+++ vars/manoj.groovy
@@ -6,11 +6,11 @@ def build()
 {
   sh "mvn package"
 }
-def deploy("jobname","ip","appname")
+def deploy(jobname,ip,appname)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
-def runtesting()
+def runtesting(jobname)
 {
    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
